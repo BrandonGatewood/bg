28a0dad1dd41fe2d1c4c13b05b70f524b5e00ddb
Brandon Gatewood <gatew2@pdx.edu>
2023-04-05 18:24:35 -0700

    updated router and hashlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,13 @@ import {useState, useEffect} from "react";
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import githubIcon from '../assets/img/github.svg';
 import linkedinIcon from '../assets/img/linkedin.svg';
+import { HashLink } from 'react-router-hash-link'
+import {
+    BrowserRouter as Router
+} from  'react-router-dom'
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('Home');
+    const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
    
     useEffect(() => {
@@ -25,35 +29,39 @@ export const NavBar = () => {
     }
 
     return(
-        <Navbar expand='md' className={scrolled ? 'scrolled': ''}>
-            <Container>
-                <Navbar.Brand href='/'>
-                    Brandon Gatewood
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'>
-                    <span className='navbar-toggler-icon'></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>
-                            Home
-                        </Nav.Link>
-                        <Nav.Link href="#experience" className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('experience')}>
-                            Experience
-                        </Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('project')}>
-                            Projects
-                        </Nav.Link>
-                    </Nav>
-                    <span classname='navbar-text'>
-                        <div classname='social-icon'>
-                            <a href="/#"><img src={githubIcon} alt=""/></a>
-                            <a href="/#"><img src={linkedinIcon} alt=""/></a>
-                        </div>
-                        <button className="vvd"><span>Let's Connect</span></button>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <Router>
+            <Navbar expand='md' className={scrolled ? 'scrolled': ''}>
+                <Container>
+                    <Navbar.Brand href='/'>
+                        Brandon Gatewood
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav'>
+                        <span className='navbar-toggler-icon'></span>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='me-auto'>
+                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href="#experience" className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('experience')}>
+                                Experience
+                            </Nav.Link>
+                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>
+                                Projects
+                            </Nav.Link>
+                        </Nav>
+                        <span classname='navbar-text'>
+                            <div classname='social-icon'>
+                                <a href="/#"><img src={githubIcon} alt="Github Icon"/></a>
+                                <a href="/#"><img src={linkedinIcon} alt="Linkedin Icon"/></a>
+                            </div>
+                            <HashLink to='#Connect'>
+                                <button className="vvd"><span>Let's Connect</span></button>
+                            </HashLink>
+                        </span>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Router>
     )
 }
