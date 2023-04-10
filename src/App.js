22ae5863208c9449d050b5b7c5440b86139aa87e
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2023-04-10 14:31:15 -0700

    Update to work with github pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Projects } from './components/Projects';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Projects />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Projects />
+      </div>
+    </BrowserRouter>
   );
 }
 
