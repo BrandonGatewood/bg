e0d0b97d1ccdd0d98bc2067c0f5dc3fec917aca7
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2023-04-10 14:36:47 -0700

    removed broswerrouter

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Projects } from './components/Projects';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <NavBar />
-        <Banner />
-        <Projects />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Projects />
+    </div>
   );
 }
 
